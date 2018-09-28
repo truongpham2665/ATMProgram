b2b5b74cbc4b868863bf4d1fc46a985c450ad31b
Tran Thanh <tranthanhkma@gmail.com>
2018-09-28 18:13:09 +0700

    fix

--- RestAPI/src/main/java/com/homedirect/RestApiApplication.java
+++ RestAPI/src/main/java/com/homedirect/RestApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApiApplication.class, args);
+	public static void main(String[] argdasdasdasads) {
+		SpringApplication.run(RestApiApplication.class, argdasdasdasads);
 	}
 }
