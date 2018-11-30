b68195da2a3291b0b00f9a24674ec6ce34c746a5
Tu Nguyen The (HomeDirect) <tu.nguyenthe@homedirect.com.vn>
2018-11-30 09:57:21 +0700

    test git

--- RestAPI/src/main/java/com/homedirect/controller/AbstractController.java
+++ RestAPI/src/main/java/com/homedirect/controller/AbstractController.java
@@ -20,36 +20,6 @@ public abstract class AbstractController<P> {
 	
 	protected @Autowired P processor;
 	
-//	protected <O> ATMResponse<O> toResponse(O data) {             //(***)
-//		ATMResponse<O> response = new ATMResponse<O>();
-//		if (data instanceof ATMException) {
-//			ATMException e = (ATMException) data;
-//			response.setCode(e.getCode());
-//			response.setMessage(e.getMessage());
-//			if (logger.isDebugEnabled()) {
-//				logger.debug(e);
-//				logger.error(e.getMessage());
-//			}
-//			return response;
-//		}
-//
-//		if (data instanceof Exception) {
-//			Exception e = (Exception) data;
-//			response.setCode(ErrorCode.UNKNOWN);
-//			response.setMessage(e.getMessage());
-//			if (logger.isDebugEnabled()) {
-//				logger.debug(e);
-//				logger.error(e.getMessage());
-//			}
-//			return response;
-//		}
-//
-//		response.setCode(ErrorCode.SUCCESS);
-//		response.setMessage(ErrorCode.SUCCESS_MES);
-//		response.setData(data);
-//		return response;
-//	}
-	
 	protected <T, R> ATMResponse<R> apply(T t, ExecutorFunction<T, R> executor) {
 		try {
 			R data = executor.execute(t);
